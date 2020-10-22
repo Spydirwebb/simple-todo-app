ace76a593a6534e32e9be82881881405378d4caa
Jeff W <spydirwebb@yahoo.com>
2020-10-22 14:22:21 -0700

    Updates

---
+++ precache-manifest.1bc4bc83a227e9b51077df4877a5c683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd80fc1422ab3e7fa128afca3c40755",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "8a20bc4f64cd8f94fe50",
+    "url": "/simple-todo-app/static/css/main.ac63b6fb.chunk.css"
+  },
+  {
+    "revision": "c529addd384c615913dc",
+    "url": "/simple-todo-app/static/js/2.f77f730d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo-app/static/js/2.f77f730d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a20bc4f64cd8f94fe50",
+    "url": "/simple-todo-app/static/js/main.e5e4cbad.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
